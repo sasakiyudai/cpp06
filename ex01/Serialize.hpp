0a9008ebbfcfc6075fb17863ebb8ea56fb9234cb
Your Name <you@example.com>
2021-05-11 20:22:48 +0900

    pragma導入

--- ex01/Serialize.hpp
+++ ex01/Serialize.hpp
@@ -3,12 +3,14 @@
 
 #include <string>
 
+#pragma pack(push, 1)
 typedef struct Data
 {
 	std::string s1;
 	int n;
 	std::string s2;
 }				Data;
+#pragma pack(pop)
 
 int randomNum(void);
 std::string randomStr(void);
